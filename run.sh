1c7d3340b056e556862fc37ad8a5a8e36a6802b3
Rob Everest <rob.everest@gmail.com>
2020-08-15 13:08:59 +1000

    run.sh is passed additional arguments we have to worry about

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${currentDir}/base.sh # Get variables from base.
 
-udocker run --containerauth --nobanner $@ "$CONTAINER_ID" /bin/bash < "${1}"
+udocker run --containerauth --nobanner "${@:1:$#-2}" "$CONTAINER_ID" /bin/bash < "${@: -2: 1}"
 if [ $? -ne 0 ]; then
     # Mark the build as failure in GitLab CI.
     exit $BUILD_FAILURE_EXIT_CODE
